121f1cbf23643412e83b60b38f3dd7dfabb71cc9
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2024-07-24 14:43:12 +0700

    Update aws-session-manager-plugin.rb

--- Formula/aws-session-manager-plugin.rb
+++ Formula/aws-session-manager-plugin.rb
@@ -3,7 +3,6 @@
 class AwsSessionManagerPlugin < Formula
     desc "Official Amazon AWS session manager plugin"
     homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
-    version "1.2.463.0"
     livecheck do
       url "https://docs.aws.amazon.com/systems-manager/latest/userguide/plugin-version-history.html"
       regex(%r{<td tabindex="-1">(\d+(?:\.\d+)+)</td>}i)
@@ -11,7 +10,7 @@ class AwsSessionManagerPlugin < Formula
 
     depends_on :linux
     
-    url "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb"
+    url "https://s3.amazonaws.com/session-manager-downloads/plugin/1.2.463.0/ubuntu_64bit/session-manager-plugin.deb"
     sha256 "cc58fc31e2239230336b243fa97bd63a7202068dd7ce8470eaf654c1928c10a8"
     
     def install
