6a47abe3743ae3183fba3570c0433885ce9b5f32
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2024-07-24 15:20:44 +0700

    Chatgpt to the rescue

--- Formula/android-platform-tools.rb
+++ Formula/android-platform-tools.rb
@@ -15,24 +15,11 @@ class AndroidPlatformTools < Formula
     sha256 "823d270be01444828ccd09e94209ff394fb8a8457243e1c15689fe0544acbc72"
     
     def install
-        system "unzip", "platform-tools_r35.0.1-linux.zip"
-        bin.install "platform-tools/adb"
-        bin.install "platform-tools/etc1tool"
-        bin.install "platform-tools/fastboot"
-        bin.install "platform-tools/hprof-conv"
-        bin.install "platform-tools/make_f2fs"
-        bin.install "platform-tools/make_f2fs_casefold"
-        bin.install "platform-tools/mke2fs"
-        prefix.install_metafiles
-    end
-
+        prefix.install Dir["*"] # Install all files into the prefix directory
+        bin.install Dir["platform-tools/*"].select { |f| File.executable? f }
+      end
+    
     test do
-        system bin/"adb"
-        system bin/"etc1tool"
-        system bin/"fastboot"
-        system bin/"hprof-conv"
-        system bin/"make_f2fs"
-        system bin/"make_f2fs_casefold"
-        system bin/"mke2fs"
+        system "#{bin}/adb", "version"
     end
 end
