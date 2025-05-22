f2ddf8dc501e2c303b0b1de837a659206108e085
nhubao <baoprokute1234@gmail.com>
2025-05-22 18:00:36 +0700

    Try fixing GO111MODULE

--- Formula/captive-browser.rb
+++ Formula/captive-browser.rb
@@ -25,7 +25,7 @@ class CaptiveBrowser < Formula
   def install
     # Build the captive-browser binary
     ENV["CGO_ENABLED"] = "0"
-    ENV["GO111MODULE"] = "off"
+    ENV["GO111MODULE"] = "auto"
     system "go", "build", "-ldflags", "-s -w -buildid=", "-trimpath", "-o", "captive-browser", "."
     bin.install "captive-browser"
 
