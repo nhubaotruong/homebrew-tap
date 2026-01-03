ca943b873ae9dd6eff02fcb855fdd8769a03d12c
nhubaotruong <baoprokute1234@gmail.com>
2026-01-03 18:17:56 +0700

    Fix build

--- Casks/http-toolkit.rb
+++ Casks/http-toolkit.rb
@@ -20,7 +20,7 @@ cask "http-toolkit" do
     strategy :github_latest
   end
 
-  binary "HttpToolkit-#{version}-linux-#{arch}/httptoolkit"
+  binary "httptoolkit"
 
   postflight do
     FileUtils.mkdir_p "#{Dir.home}/.local/share/applications"
@@ -36,7 +36,7 @@ cask "http-toolkit" do
       Name=HTTP Toolkit
       Comment=HTTP(S) debugging proxy, analyzer, and client
       GenericName=HTTP Debugger
-      Exec=#{staged_path}/HttpToolkit-#{version}-linux-#{arch}/httptoolkit %U
+      Exec=#{staged_path}/httptoolkit %U
       Icon=#{Dir.home}/.local/share/icons/httptoolkit.svg
       Terminal=false
       StartupNotify=true
