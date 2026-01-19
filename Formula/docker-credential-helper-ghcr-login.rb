05b175c4ef33bf98df42890c28698e741e9236e9
nhubaotruong <baoprokute1234@gmail.com>
2026-01-19 20:21:27 +0700

    Update format

--- Formula/docker-credential-helper-ghcr-login.rb
+++ Formula/docker-credential-helper-ghcr-login.rb
@@ -5,18 +5,17 @@ class DockerCredentialHelperGhcrLogin < Formula
       revision: "706dc0da92388a6993dbe5c32090beb550585b30"
   version "706dc0d"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/nhubaotruong/homebrew-tap/releases/download/docker-credential-helper-ghcr-login-706dc0d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d2020080f439f1c81673de9dc673abe6e7e45328a5a823ebd0577ea8a6768f06"
-  end
-
   head "https://github.com/bradschwartz/docker-credential-ghcr-login.git",
        branch: "main"
 
   livecheck do
     url :head
     strategy :git
+  end
+
+  bottle do
+    root_url "https://github.com/nhubaotruong/homebrew-tap/releases/download/docker-credential-helper-ghcr-login-706dc0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d2020080f439f1c81673de9dc673abe6e7e45328a5a823ebd0577ea8a6768f06"
   end
 
   depends_on "go" => :build
