f997fdfc075b073e052bbfa90429a4bc11a78403
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2026-06-11 02:38:30 +0000

    http-toolkit 1.26.1

--- Casks/http-toolkit.rb
+++ Casks/http-toolkit.rb
@@ -2,9 +2,9 @@ cask "http-toolkit" do
   arch arm: "arm64", intel: "x64"
   os linux: "linux"
 
-  version "1.26.0"
-  sha256 arm64_linux:  "a152a638a32142c3fbb14ace3a1a35afedb4992e45e3a564e990a17b28423b51",
-         x86_64_linux: "d84c47fd9b50124c35d05b4aaaf0ce53e1aac69aef9b616573d24b9e5ada9982"
+  version "1.26.1"
+  sha256 arm64_linux:  "f6df147b5685a3c3718e2b0716cdf2a5336a5cbdf9935bc05588627fbc7d5ccb",
+         x86_64_linux: "3b13cffcafa38fb871241bb81233d867ceb7dc2a83b188da8261f5865ee4fc1d"
 
   url "https://github.com/httptoolkit/httptoolkit-desktop/releases/download/v#{version}/HttpToolkit-#{version}-linux-#{arch}.zip",
       verified: "github.com/httptoolkit/httptoolkit-desktop/"
