c5790a0e9d7476224d23e301585b964607192f19
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2026-08-24 03:47:58 +0000

    docker-credential-ghcr-login 0.2.0

--- Casks/docker-credential-ghcr-login.rb
+++ Casks/docker-credential-ghcr-login.rb
@@ -2,11 +2,11 @@ cask "docker-credential-ghcr-login" do
   arch arm: "arm64", intel: "amd64"
   os linux: "linux"
 
-  version "0.1.0"
+  version "0.2.0"
 
   on_linux do
-    sha256 arm64_linux:  "25678d9e7d052e10afff4d14e37ed4b4833bda1a65049442ad98d820b9e978f3",
-           x86_64_linux: "38970351fa919cc1dc3b94f49d6f46e6f37d7508b3cf025924de3ff5747a9e7a"
+    sha256 arm64_linux:  "e866d63d3335cdc67b7e0ed16e2dfe84500826b13677bbd9fc42009a2c23a1d9",
+           x86_64_linux: "01e7657acced71105fe22ce4394f323d2dca3a915c881d8711b67e555c57e070"
 
     url "https://github.com/bradschwartz/docker-credential-ghcr-login/releases/download/v#{version}/docker-credential-ghcr-login_#{version}_linux_#{arch}.tar.gz",
         verified: "github.com/bradschwartz/docker-credential-ghcr-login/"
