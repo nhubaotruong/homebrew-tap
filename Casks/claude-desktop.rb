18f1b5a03e539ba2ca072579912565b8e78461cc
Như Bảo Trương <28810481+nhubaotruong@users.noreply.github.com>
2026-09-09 07:44:52 +0000

    claude-desktop 3.2.4+claude1.49585.0

--- Casks/claude-desktop.rb
+++ Casks/claude-desktop.rb
@@ -3,9 +3,9 @@ cask "claude-desktop" do
   os linux: "linux"
 
   on_linux do
-    version "3.2.4+claude1.46388.2"
-    sha256 arm64_linux:  "b944a2154528815bb476bfb1e7091f8a3e329b3c67f6c11de8d41668a603bd9e",
-           x86_64_linux: "98bf54e85e4916068c4281459b0f0431d8ff68034773f3ee98311d7206566ab1"
+    version "3.2.4+claude1.49585.0"
+    sha256 arm64_linux:  "8a3ae3e007c36a213a16b1b5394e3a0c836baf0cf88a08491e8202952ae27020",
+           x86_64_linux: "e0b4ce6313943a69e9bd61749a293bfca241b4dfb37f88df6411b4b69ec67a40"
 
     claude_version = version.split("+claude")[1]
     url "https://github.com/aaddrick/claude-desktop-debian/releases/download/v#{version.gsub("+", "%2B")}/claude-desktop_#{claude_version}_#{arch}.deb"
